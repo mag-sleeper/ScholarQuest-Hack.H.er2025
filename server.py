f5880bdb3278ae3b0b88dc0f153933f22cf93281
mag-sleeper <xiangweichen@umass.edu>
2025-02-15 20:39:04 -0500

    update all gen ed course

--- server.py
+++ server.py
@@ -3,6 +3,42 @@ from canvas_calendar import fetch_calendar_events
 
 app = Flask(__name__)
 #store assignments data
+
+graduation_requirements = {
+    "general_education": {
+        "writing": {
+            "courses": ["ENGLWRIT112", "Junior Year Writing"]
+        },
+        "math": {
+            "courses": ["R1 - Basic Math", "R2 - Analytical Reasoning"]
+        },
+        "science": {
+            "courses": ["BS - Biological Science", "PS - Physical Science"]
+        },
+        "social_world": {
+            "literature_arts": {
+                "courses": ["AL - Literature", "AT - Theatre", "ART - Design"]
+            },
+            "historical_studies": {
+                "courses": ["HS - Historical Studies"]
+            },
+            "social_behavioral": {
+                "courses": ["SB - Social and Behavioral Sciences"]
+            },
+            "additional_social_world": {
+                "courses": ["AL/AT/SB or Interdisciplinary (I or SI)"]
+            },
+            "diversity": {
+                "us_diversity": ["DU - Diversity in the US"],
+                "global_diversity": ["DG - Global Diversity"]
+            }
+        },
+        "integrative_experience": {
+            "courses": ["3 credits (Major Specific)"]
+        }
+    }
+}
+
 assignments_data = {}
 
 @app.route('/assignments', methods=['GET','POST'])
@@ -19,6 +55,12 @@ def get_assignment():
         }
         print("📌 DEBUG: assignments_data =", assignments_data)
         return jsonify(assignment_data)
+
+def remaining_gen_ed(requirements, completed_courses, current_courses, remaining_courses):
+    remaining_courses = {}
+    all_course = completed_courses + current_courses
+    for category
+
 
 
 if __name__ == '__main__':
