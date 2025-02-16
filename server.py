3ef3187b6290644a9735f2330423b0e0d4d204fe
mag-sleeper <xiangweichen@umass.edu>
2025-02-15 20:49:43 -0500

    server updated, remaining gen ed data added

--- server.py
+++ server.py
@@ -1,44 +1,34 @@
 from flask import Flask, request, jsonify
 from canvas_calendar import fetch_calendar_events
+import json
 
 app = Flask(__name__)
-#store assignments data
 
-graduation_requirements = {
-    "general_education": {
-        "writing": {
-            "courses": ["ENGLWRIT112", "Junior Year Writing"]
-        },
-        "math": {
-            "courses": ["R1 - Basic Math", "R2 - Analytical Reasoning"]
-        },
-        "science": {
-            "courses": ["BS - Biological Science", "PS - Physical Science"]
-        },
-        "social_world": {
-            "literature_arts": {
-                "courses": ["AL - Literature", "AT - Theatre", "ART - Design"]
-            },
-            "historical_studies": {
-                "courses": ["HS - Historical Studies"]
-            },
-            "social_behavioral": {
-                "courses": ["SB - Social and Behavioral Sciences"]
-            },
-            "additional_social_world": {
-                "courses": ["AL/AT/SB or Interdisciplinary (I or SI)"]
-            },
-            "diversity": {
-                "us_diversity": ["DU - Diversity in the US"],
-                "global_diversity": ["DG - Global Diversity"]
-            }
-        },
-        "integrative_experience": {
-            "courses": ["3 credits (Major Specific)"]
-        }
-    }
-}
+with open("graduation_requirements.json") as f:
+    graduation_requirements = json.load(f)
 
+def check_gen_ed_requirements(req, completed_cor, current_cor, remaining_cor):
+    remaining_cor = {}
+    
+    all_courses = completed_cor + current_cor
+    
+    for category, category_data in req["general_education"].items():
+        unsatisfied_courses = [course for course in category_data["courses"] if course not in all_courses]
+        if unsatisfied_courses:
+            remaining_cor[category] = unsatisfied_courses
+    
+    return remaining_cor
+
+@app.route('/remaining_gen_ed', methods=['POST'])
+def remaining_gen_ed():
+    data = request.json
+    completed_cor = data.get('completed_courses', [])
+    current_cor = data.get('current_semester_courses', [])
+    
+    remaining_cor = check_gen_ed_requirements(graduation_requirements, completed_cor, current_cor, remaining_cor)
+    
+    return jsonify(remaining_cor)
+    
 assignments_data = {}
 
 @app.route('/assignments', methods=['GET','POST'])
